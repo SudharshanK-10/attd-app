757e601c3b5c8f1f380d31636c52d9286267983c
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-13 21:30:22 +0530

    register

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM faculty');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
