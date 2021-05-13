e0c08bffd61b889895b9ac6e8ce198338c9bec7e
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-13 20:56:57 +0530

    register

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
