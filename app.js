bfe3377b499d9010ab77cc4c09fef5a9eeb62c52
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-13 21:38:00 +0530

    register

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const { Pool } = require('pg');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
