acab6d93087420bf4bd35e6195dd0d94ce674b6b
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-13 21:10:52 +0530

    register

--- app.js
+++ app.js
@@ -14,8 +14,16 @@ app.post('/faculty', function(req, res){
     const first_name = req.body.first_name;
     const last_name = req.body.last_name;
     const password = req.body.password;
+    
     console.log(`First name: ${first_name}, Last name: ${last_name}, Password: ${password}`);
-    res.send('request received!');
+    //res.send('request received!');
+    
+    const obj  = {
+   'first_name'  : first_name,
+   'last_name'	 : last_name,
+   'password'    : password
+   };
+   res.send(JSON.stringify(obj));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
