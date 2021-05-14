078f7cd8f957b05f15b74351f2187db18eec8a0b
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-14 09:49:44 +0530

    register

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/db', async (req, res) => {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM faculty;');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('/db', results );
+      res.send(JSON.stringify(result));
       client.release();
     } catch (err) {
       console.error(err);
