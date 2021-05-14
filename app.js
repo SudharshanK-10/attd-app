6e42e34f779ea7a09e7e0d512268914c6d8e5e49
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-14 14:21:55 +0530

    logged

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post('/logged', async(req, res) => {
       var result = await client.query(text,values);
       var faculty = { 'faculty': (result) ? result.rows : null};
       //res.send(faculty_id+" "+faculty.first_name+" "+faculty.last_name+" "+faculty.password+" ");
-      res.send(faculty);
+      //res.send(faculty);
       res.render('logged',{'given': snd_obj,'fetched': faculty});
       client.release();
     } catch (err) {
