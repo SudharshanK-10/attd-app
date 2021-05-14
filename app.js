1de4c09a2272232283a25eff4c2300d009876795
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-14 13:09:32 +0530

    logged

--- app.js
+++ app.js
@@ -38,9 +38,12 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/',function(req,res) {
+
+//login page
+/*app.get('/index.html',function(req,res) {
 	res.sendFile(path.join(__dirname + '/login.html'));
 });
+*/
 
 app.get(path.join(__dirname+'/login.html'),function(req,res) {
 	res.sendFile(path.join(__dirname+'/login.html'));
