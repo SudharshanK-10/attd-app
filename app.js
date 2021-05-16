cb24fe88cfc6b1157358364f19f1055eb7a0e396
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-16 15:54:02 +0530

    register

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 const saltrounds = 7;
-var hashed_password = "";
+
 
 // for parsing application/xwww-
 app.use(express.urlencoded({ extended: true }));
@@ -59,26 +59,10 @@ app.get('/login.html',function(req,res) {
 app.post('/faculty', async(req, res) => {
     const first_name = req.body.first_name;
     const last_name = req.body.last_name;
-    var password = req.body.password;
+    const password = bcrypt.hashSync(req.body.password,saltrounds);
     const email = req.body.email;
     const college = req.body.college;
 
-    bcrypt.genSalt(saltrounds, function (err, salt) {
-  if (err) {
-    throw err;
-  } else {
-    bcrypt.hash(password, salt, function(err, hash) {
-      if (err) {
-        throw err;
-      }
-      else {
-          hashed_password = hash;
-          console.log(hashed_password);
-     }
-        });
-      }
-    });
-    password = hashed_password;
     console.log(`First name: ${first_name}, Last name: ${last_name}, Password: ${password}, Email: ${email}, college: ${college}`);
     //res.send('request received!');
 
@@ -133,17 +117,7 @@ app.post('/logged', async(req, res) => {
       password = faculty[0].password;
 
       //decrypt the Password
-      const ok = 0;
-
-      bcrypt.compare(given.password, password, function(err, isMatch) {
-           if (err) {
-                throw err;
-           } else if (!isMatch) {
-                ok=0;
-           } else {
-               ok=1;
-           }
-      });
+      const ok = bcrypt.comapareSync(given.password,password);
 
       if(email==given.email && ok){
            res.render('logged',{given: faculty});
