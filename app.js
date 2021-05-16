d9e6ebff9af9476aeca75105cef9ff1698edccb7
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-16 11:26:25 +0530

    register

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.post('/logged', async(req, res) => {
     try {
       var client = await pool.connect();
       var result = await client.query(text,values);
-      //var faculty = { 'faculty': (result) ? result.rows : null};
+      var faculty = { 'faculty': (result) ? result.rows : null};
       var faculty = result;
       //res.send(faculty_id+" "+faculty.first_name+" "+faculty.last_name+" "+faculty.password+" ");
       //res.send(faculty);
