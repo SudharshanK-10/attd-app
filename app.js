9abefb6603e71b4e60d37543cf8495a83f5c43c1
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-16 15:46:19 +0530

    register

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 const saltrounds = 7;
-const hashed_password = "";
+var hashed_password = "";
 
 // for parsing application/xwww-
 app.use(express.urlencoded({ extended: true }));
