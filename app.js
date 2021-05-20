150241d0f5e37b9316019b001c58ddc7d74c7596
Sudharshan <sudharshan@DESKTOP-O3QSBC4.localdomain>
2021-05-20 13:20:39 +0530

    register

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 //app.use(express.static(__dirname + '/logged'));
 app.use(fileUpload());
+app.use(express.static('public/img'));
 /*app.use(fileUpload({
     createParentPath: true
 }));
